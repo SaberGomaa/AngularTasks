83fe11165e4cd2a1da0d1d8707182811bc3015ca
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-17 09:39:09 +0200

    Generate Comps

--- AngularTasks/src/app/app.module.ts
+++ AngularTasks/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from '.e:/FrontEnd/angular/AngularTs/AngularTasks/AngularTasks/src/Product/Product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ProductComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
---
+++ AngularTasks/src/components/orderMaster/orderMaster.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-orderMaster',
+  templateUrl: './orderMaster.component.html',
+  styleUrls: ['./orderMaster.component.css']
+})
+export class OrderMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
