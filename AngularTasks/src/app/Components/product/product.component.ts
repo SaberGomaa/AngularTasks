684e57e3ecae72b4c22fa798b7d17f04ac3a4584
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-19 16:58:48 +0200

    View Products

--- AngularTasks/src/app/Components/product/product.component.ts
+++ AngularTasks/src/app/Components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './../../Models/IProduct';
 
 @Component({
   selector: 'app-product',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+ProductList: IProduct[];
 
-  constructor() { }
+  constructor() { 
+    this.ProductList = [
+      {Id : 1 , Name : 'Lab Dell' ,Price : 100 , Quantity : 10 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'}
+    ]
+  }
 
   ngOnInit(): void {
   }
