1ab207ec14b02f5c6ee7a9aca20944ae7e77e6d5
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-19 20:30:31 +0200

    CAT

--- AngularTasks/src/app/Components/product/product.component.ts
+++ AngularTasks/src/app/Components/product/product.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './../../Models/IProduct';
+import { ICategory } from './../../Models/ICategory';
 
 @Component({
   selector: 'app-product',
@@ -7,15 +8,27 @@ import { IProduct } from './../../Models/IProduct';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+
 ProductList: IProduct[];
+CatList : ICategory[];
 
   constructor() { 
-    this.ProductList = [
-      {Id : 1 , Name : 'Lab Dell' ,Price : 100 , Quantity : 10 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'},
-      {Id : 1 , Name : 'Lab Dell' ,Price : 100 , Quantity : 10 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'},
-      {Id : 1 , Name : 'Lab Dell' ,Price : 100 , Quantity : 10 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'},
 
-    ]
+    this.ProductList = [
+      {Id : 1 , Name : 'Dell' ,Price : 1040 , Quantity : 10 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'},
+      {Id : 2 , Name : 'Hp' ,Price : 1485 , Quantity : 20 ,CatId : 1 ,Img : 'https://fakeimg.pl/50x50/'},
+      {Id : 3 , Name : 'OPPO' ,Price : 4894 , Quantity : 45 ,CatId : 2 ,Img : 'https://fakeimg.pl/50x50/'},
+      {Id : 4 , Name : 'Samsung' ,Price : 4800 , Quantity : 81 ,CatId : 2 ,Img : 'https://fakeimg.pl/50x50/'},
+      {Id : 5 , Name : 'LG' ,Price : 4640 , Quantity : 32 ,CatId : 3 ,Img : 'https://fakeimg.pl/50x50/'},
+      {Id : 6 , Name : 'Toshiba' ,Price : 1540 , Quantity : 17 ,CatId : 3 ,Img : 'https://fakeimg.pl/50x50/'},
+    ];
+
+    this.CatList =[
+      {Id : 1 , Name : 'Labtops'},
+      {Id : 2 , Name : 'Mobile'},
+      {Id : 3 , Name : 'TVs'},
+    ];
+
   }
 
   ngOnInit(): void {
